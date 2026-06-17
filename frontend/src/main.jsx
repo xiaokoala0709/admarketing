1ee3e1b2bad62f1bddf300cf1bd4b4d3abbe5ba8
xiaokoala0709 <xiaokoala0709@gmail.com>
2026-06-17 22:42:54 +0800

    Update main.jsx
    增加登录页

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { ClerkProvider } from '@clerk/react'
+
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey={publishableKey} afterSignOutUrl="/">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>,
 )
